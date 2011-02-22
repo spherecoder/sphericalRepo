d1657cd1ec96fb4972a9eab3a2624ca5dd05e57d
Ole Moeller-Nilsson <ole.moeller.nilsson@googlemail.com>
2011-02-22 18:33:26 +0100

    -- some more changes

--- TestFramework/otools/treeSelectDrop.js
+++ TestFramework/otools/treeSelectDrop.js
@@ -7,6 +7,7 @@ dojo.require("dijit.layout.BorderContainer");
 dojo.require("dojo.data.ItemFileReadStore");
 dojo.require("dojo.data.ItemFileWriteStore");
 dojo.require("dijit.Tree");
+dojo.require("dijit.form.TextBox");
 dojo.require("dijit.tree.dndSource");
 dojo.require("dojo.parser");
 dojo.require("dojo.fx");
@@ -14,23 +15,42 @@ dojo.require("dojo.dnd.common");
 dojo.require("dojo.dnd.Source");
 
 function treeSelectDrop_dndAccept(source,nodes){
+	if ( this.id == null ) return false;
     if (this.id.search("treeSelectDropTargetID") != -1) {
         var a = source.getItem(nodes[0].id).data;
-        if (a.isExpandable == false) {
-            return true;
+        if ( a.isTreeNode == true ) {
+        	if (a.isExpandable == false) {
+        		return true;
+        	}
+        }
+        else {
+        	return true;
         }
         return false;
     }
     return false;
 }
 function treeSelectDrop_no_own_drop(sources,nodes,position) {
-	return false;
-}
-function treeSelectDrop_droppedIt(source,nodes,copy) {
-    if (this.id.search("treeSelectDropTargetIdD") != -1) {
+    if (source.id.search("treeSelectDropTargetID") != -1) {
         var a = source.getItem(nodes[0].id).data;
         if (a.isExpandable == false) {
             this.insertNodes(false, [a.label ]);
+        }
+    }
+}
+
+function treeSelectDrop_droppedIt(source,nodes,copy) {
+    if (this.id.search("treeSelectDropTargetID") != -1) {
+        var a = source.getItem(nodes[0].id).data;
+        if ( a.isTreeNode == true ) {
+        	if (a.isExpandable == false) {
+                this.insertNodes(false, [a.label ]);
+        		return true;
+        	}
+        }
+        else {
+            this.insertNodes(false, [a ]);
+        	return true;
         }
     }
 }
@@ -49,13 +69,7 @@ function treeSelectDrop_fileSelect_tree(urlin,queryin,rootLabelin) {
    var tree = new dijit.Tree({
        model: treeModel,
        dndController: dijit.tree.dndSource,
-       checkItemAcceptance: treeSelectDrop_no_own_drop,
-       dndParams: {checkAcceptance: treeSelectDrop_dndAccept,
-	   checkItemAcceptance: treeSelectDrop_no_own_drop,
-	   onDndDrop: treeSelectDrop_droppedIt,
-	   onDrop: treeSelectDrop_droppedIt},
-	   onDndDrop: treeSelectDrop_droppedIt,
-	   onDrop: treeSelectDrop_droppedIt
+       checkItemAcceptance: treeSelectDrop_no_own_drop
    });
    return tree;
 }
@@ -79,12 +93,16 @@ function treeSelectDrop_initCategories(categories) {
         td.style.height = "80px";
         td.innerHTML = categories[i].name;
         var td2 = document.createElement("td");
-        td2.id = "categoryTarget" + i.toString();
         td2.style.border = "1px solid";
-        td2.style.width = "100px";
+        td2.style.width = "200px";
         td2.style.height = "80px";
-		td2.category = categories[i].name;
-        //var dropTarget = new dojo.dnd.Target(td2, {id: "treeSelectDropTargetID" + i.toString(), checkAcceptance: treeSelectDrop_dndAccept, onDrop: treeSelectDrop_droppedIt} );
+		var divel = document.createElement("div");
+		divel.id = "categoryTarget" + i.toString();
+        divel.category = categories[i].name;
+        divel.style.width = "200px";
+        divel.style.height = "80px";
+        divel.style.overflow = "auto";
+        td2.appendChild(divel);
         tr.appendChild(td);
         tr.appendChild(td2);
         tab.appendChild(tr);
@@ -99,28 +117,18 @@ function treeSelectDrop_new( titlein, urlin,queryin,rootLabelin, categories ) {
     var tagPane = new dijit.layout.ContentPane({region: "center"});
     var sofPane = new dijit.layout.ContentPane({id: "catPane", region: "right", splitter: "false", style: "width: 350px;"});
     var tree = treeSelectDrop_fileSelect_tree(urlin, queryin, rootLabelin);
+    var saveButton = new dijit.form.Button();
     mainPane.addChild(filePane);
     mainPane.addChild(tagPane);
     mainPane.addChild(sofPane);
     tree.placeAt(filePane.domNode);
-    treeSelectDrop_initCategories(categories)
-    //var tagslist = getSofTagList(insname,recipename);
+    treeSelectDrop_initCategories(categories);
+    document.body.appendChild(mainPane.domNode);
+    for (var i = 0; i < categories.length; i++) {
+    	var el = document.getElementById("categoryTarget" + i.toString());
+    	var dropTarget = new dojo.dnd.Source(el, {id: "treeSelectDropTargetID" + i.toString(), checkAcceptance: treeSelectDrop_dndAccept, onDrop: treeSelectDrop_droppedIt});
+    	dropTarget.category = categories[i].name;
+    }
     return mainPane;
 }
 
-dojo.addOnLoad(function() {
-	var cats = [{
-		name: "a"
-	}];
-    var ts = treeSelectDrop_new("Aha", "countries.json", {type: 'continent'},"rooty",cats);
-    var accordionPane = new dijit.layout.AccordionContainer({style: "width: 800px; height: 600px;"});
-    var generalPane = new dijit.layout.ContentPane({title: "General", content: "Hello!"});
-    var sofPane =  new dijit.layout.ContentPane({title: "GSOFPane", content: "Hello!"});
-    var paramPane = new dijit.layout.ContentPane({title: "Parameters", content: "Hello!"});
-    accordionPane.addChild(generalPane);
-    accordionPane.addChild(sofPane);
-    accordionPane.addChild(paramPane);
-    accordionPane.addChild(ts);
-    document.body.appendChild(accordionPane.domNode);
-    accordionPane.startup();   
-    });
