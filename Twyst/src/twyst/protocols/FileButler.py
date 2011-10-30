2e266f2fc468109cc96390b22d3d79fc1e18bd12
Ole Moeller-Nilsson <ole.moeller.nilsson@googlemail.com>
2011-10-30 09:50:28 +0100

    A new commit

--- Twyst/src/twyst/protocols/FileButler.py
+++ Twyst/src/twyst/protocols/FileButler.py
@@ -21,56 +21,35 @@ class FileButler(TwystObject,Protocol):
     current_filename = ""
     CHUNK_SIZE = 2 ** 14
     file = None
-    PATH_SEP = "####"
-    MODE_RECFILE = 0
-    MODE_SENDFILE = 0
-    mode = MODE_RECFILE
+
+    def setFilename(self,filename):
+        self.current_filename = filename
+    
     def connectionMade(self):
-        self.factory.numProtocols = self.factory.numProtocols+1
-        self.mode = FileButler.MODE_RECFILE 
+        self.factory.numProtocols = self.factory.numProtocols + 1
         if self.factory.numProtocols > 100:
             self.transport.write("Too many connections, try later") 
             self.transport.loseConnection()
+        if self.factory.filename:
+            self.setFilename(self.factory.filename)
 
     def connectionLost(self, reason):
         self.factory.numProtocols = self.factory.numProtocols-1
 
-    def sendReceiveRequest(self,subdir,filename):
-        self.transport.write("PLEASERECEIVE" + subdir + FileButler.PATH_SEP + filename)
-        self.mode = self.MODE_SENDFILE
-        
-    def sendSendRequest(self,subdir,filename):
-        self.file = open(self.current_filename,"w")
-        self.transport.write("PLEASESEND" + subdir + FileButler.PATH_SEP + filename)
-        self.mode = self.MODE_RECFILE
-    
     def dataReceived(self, data):
-        print data
-        if data.startswith("SEND") or data.startswith("PLEASESEND"):
-            if self.file:
-                    data = self.file.read(self.CHUNK_SIZE)
-                    if not data:
-                        self.file.close()
-                        self.file = None
-                        self.transport.loseConnection()
-                    self.mode = FileButler.MODE_SENDFILE
-                    self.transport.write(data)
-                    
-            else:
-                try:
-                    self.current_filename = data[7:].strip()
-                    self.file = open(self.current_filename)
-                    dat = self.file.read(self.CHUNK_SIZE)
-                    self.mode = FileButler.MODE_SENDFILE
-                    self.transport.write(dat)
-                except Exception as e:
-                    pass
-        elif data.sartswith("PLEASERECEIVE"):
-            subdir,filename = data[13:].split(FileButler.PATH_SEP) 
-            self.mode = FileButler.MODE_RECFILE
-            self.transport.write("SEND")            
-        elif self.mode == FileButler.MODE_RECFILE:
-            if self.file:
-                self.file.write(data)
-        else:
+        if self.file:
+            data = self.file.read(self.CHUNK_SIZE)
+            if not data:
+                self.file.close()
+                self.file = None
+                self.transport.loseConnection()
             self.transport.write(data)
+        elif self.current_filename == None:
+            return
+        else:
+            try:
+                self.file = open(self.current_filename)
+                dat = self.file.read(self.CHUNK_SIZE)
+                self.transport.write(dat)
+            except Exception as e:
+                pass
